9449429d3e89b54f012c9e72c6fd63b7460cb6f7
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2020-08-03 14:25:22 +0300

    Player doesn't slide down while on a slope

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,24 +2,10 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float VelocityX
-    {
-        set
-        {
-            if(!aiming.IsAiming)
-            {
-                newVelocity.x = value * speed;
-                if(value > 0) FlipX = false;
-                else if(value < 0) FlipX = true;
-                animator.SetBool("Running", value != 0);
-                if(value != 0) aiming.ResetRotation();
-            }
-        }
-    }
     public bool FlipX 
     {
         get => flipX;
-        set 
+        private set 
         {
             flipX = value;
             myTransform.rotation = value ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
@@ -27,19 +13,14 @@ public class PlayerController : MonoBehaviour
     }
     private bool flipX;
 
-    public bool IsGrounded
-    {
-        get
-        {
-            float distance = cc.size.y / 2 - cc.size.x / 2 + groundCheckDistance;
-            return Physics2D.CircleCast(cc.bounds.center, cc.size.x / 2, Vector2.down, distance);
-        }
-    }  
-    public bool IsMoving => rb.velocity.x != 0 || rb.velocity.y != 0;
+    public bool IsGrounded { get; private set; }  
+    public bool IsMoving => newVelocity.x != 0 || newVelocity.y != 0;
 
     [SerializeField] private float speed = 5;
-    [SerializeField] private float jumpForce = 250;
+    [SerializeField] private float jumpForce = 8;
     [SerializeField] private float groundCheckDistance = 0.05f;
+    [SerializeField] private PhysicsMaterial2D noFriction = null;
+    [SerializeField] private PhysicsMaterial2D fullFriction = null;
     
     private Transform myTransform;
     private Rigidbody2D rb;
@@ -47,11 +28,31 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private PlayerAiming aiming;
     private Vector2 newVelocity;
+    private Vector2 slopeNormalPerpendicular;
+    private bool isOnSlope;
+    private float slopeNormalDotProduct;
+    private bool isJumping;
+
+    public void SetVelocityX(float value)
+    {
+        if(!aiming.IsAiming)
+        {
+            newVelocity.x = value * speed;
+            if(isOnSlope) newVelocity.x *= 0.9f;
+            if(value > 0) FlipX = false;
+            else if(value < 0) FlipX = true;
+            animator.SetBool("Running", value != 0);
+            if(value != 0) aiming.ResetRotation();
+        }
+    }
 
     public void Jump()
     {
         if(!aiming.IsAiming && IsGrounded)
-           rb.AddForce(Vector2.up * jumpForce);
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            isJumping = true;
+        }
     }
 
     private void Awake()
@@ -61,44 +62,61 @@ public class PlayerController : MonoBehaviour
         cc = GetComponent<CapsuleCollider2D>();
         animator = GetComponent<Animator>();
         aiming = GetComponent<PlayerAiming>();
+        FlipX = false;
     }
 
 #if UNITY_EDITOR
     private void Update() 
     {
-        VelocityX = Input.GetAxisRaw("Horizontal");
+        SetVelocityX(Input.GetAxisRaw("Horizontal"));
         if(Input.GetKeyDown(KeyCode.UpArrow)) Jump();
     }
 #endif 
 
     private void FixedUpdate() 
     {
+        GroundCheck();
         SlopeCheck();
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.sharedMaterial = noFriction;
+        if(IsGrounded && !isJumping)
+        {
+            if(isOnSlope)
+            {
+                if(newVelocity.x == 0) rb.sharedMaterial = fullFriction;
+                Vector2 rayOrigin = new Vector2(cc.bounds.center.x, cc.bounds.center.y - cc.size.y / 2 + 0.2f);
+                RaycastHit2D hitFront = Physics2D.Raycast(rayOrigin, Vector2.right, 0.5f);
+                RaycastHit2D hitBack = Physics2D.Raycast(rayOrigin, Vector2.left, 0.5f);
+                if(newVelocity.x > 0 && slopeNormalDotProduct < 0 && !hitFront) newVelocity.y = 0;
+                else if(newVelocity.x < 0 && slopeNormalDotProduct > 0 && !hitBack) newVelocity.y = 0;
+                else newVelocity.Set(newVelocity.x * -slopeNormalPerpendicular.x, newVelocity.x * -slopeNormalPerpendicular.y);
+            }
+            else newVelocity.y = 0;
+        }
+        else newVelocity.y = rb.velocity.y;
         rb.velocity = newVelocity;
+        if(rb.velocity.y <= 0 || IsGrounded) isJumping = false;
+    }
+
+    private void GroundCheck()
+    {
+        float distance = cc.size.y / 2 - cc.size.x / 2 + groundCheckDistance;
+        IsGrounded = Physics2D.CircleCast(cc.bounds.center, cc.size.x / 2, Vector2.down, distance);
     }
 
     private void SlopeCheck()
     {
-        if(IsGrounded)
+        Vector2 rayOrigin = new Vector2(cc.bounds.center.x, cc.bounds.center.y - cc.size.y / 2);
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down); 
+        isOnSlope = Vector2.Angle(hit.normal, Vector2.up) > 0;
+        if(isOnSlope)
         {
-            Vector2 rayOrigin = new Vector2(cc.bounds.center.x, cc.bounds.center.y - cc.size.y / 2);
-            Vector2 rayOriginHorizontal = rayOrigin + new Vector2(0, 0.2f);
-            RaycastHit2D hitDown = Physics2D.Raycast(rayOrigin, Vector2.down); 
-            RaycastHit2D hitFront = Physics2D.Raycast(rayOriginHorizontal, Vector2.right, 0.5f);
-            RaycastHit2D hitBack = Physics2D.Raycast(rayOriginHorizontal, Vector2.left, 0.5f);
-            if(Vector2.Angle(hitDown.normal, Vector2.up) > 0)
-            {
-                float dot = Vector2.Dot(hitDown.normal, Vector2.right);
-                if((newVelocity.x > 0 && dot < 0 && !hitFront)) newVelocity.y = 0;
-                else if((newVelocity.x < 0 && dot > 0 && !hitBack)) newVelocity.y = 0;
-                else
-                {
-                    Vector2 slopeNormalPerpendicular = Vector2.Perpendicular(hitDown.normal).normalized;
-                    newVelocity.Set(newVelocity.x * -slopeNormalPerpendicular.x, newVelocity.x * -slopeNormalPerpendicular.y);
-                }
-            }
-            else newVelocity.y = 0;
+            slopeNormalDotProduct = Vector2.Dot(hit.normal, Vector2.right);
+            slopeNormalPerpendicular = Vector2.Perpendicular(hit.normal).normalized;
         }
-        else newVelocity.y = rb.velocity.y;  
     }
 }
