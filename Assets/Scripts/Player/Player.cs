39474f4f418308f2d3d16373d5060eb5a6dab96a
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2020-08-28 22:08:52 +0300

    Player has 3 lifes, player moves to the last saved position after each death, if player has no lifes - level fails

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,15 +2,21 @@
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
     private GameController gameController;
     private ExtinguishingSubstance extinguishingSubstance;
+    private Vector2 lastSavedPosition;
 
     public void PauseLevel() => gameController.PauseLevel();
 
+    public void MoveToLastSavedPosition() => rb.position = lastSavedPosition;
+
     private void Awake() 
     {
+        rb = GetComponent<Rigidbody2D>();
         gameController = GameObject.FindObjectOfType<GameController>();    
         extinguishingSubstance = transform.GetComponentInChildren<ExtinguishingSubstance>();
+        lastSavedPosition = rb.position;
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,12 +1,29 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public float LifesLeft
+    {
+        get => lifesLeft;
+        private set
+        {
+            lifesLeft = value;
+            healthText.text = lifesLeft.ToString();
+        }
+    }
+
+    [SerializeField] private TextMeshProUGUI healthText = null;
+
+    private float lifesLeft = 3;
+    private Player player;
     private GameController gameController;
 
     private void Awake()
     {
+        player = GetComponent<Player>();
         gameController = GameObject.FindObjectOfType<GameController>();
+        LifesLeft = lifesLeft;
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
@@ -16,6 +33,8 @@ public class PlayerHealth : MonoBehaviour
 
     private void Die()
     {
-        gameController.FailLevel();
+        LifesLeft--;
+        if(lifesLeft > 0) player.MoveToLastSavedPosition();
+        else gameController.FailLevel();
     }
 }
