ffcd688f526ec8b8a046887d31ec0c6293404a61
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-01-02 12:00:53 +0200

    Created Backdraft.cs instead of Explosion.cs; smoke comes out of room before backdraft

---
+++ Assets/Scripts/Miscellaneous/Backdraft.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Backdraft : MonoBehaviour
+{
+    [SerializeField] private BreakableObject breakableToListen = null;
+    [SerializeField] private ParticleSystem smoke = null;
+    [SerializeField] private float delayBeforeExplosion = 1;
+    [SerializeField] private float duration = 2;
+
+    private ParticleSystem ps;
+    private Collider2D coll;
+
+    private void Awake() 
+    {
+        ps = GetComponent<ParticleSystem>();
+        coll = GetComponent<Collider2D>();
+        coll.enabled = false;
+    }
+
+    private void OnEnable() => breakableToListen.Broken += Explode;
+
+    private void OnDisable() => breakableToListen.Broken -= Explode;
+
+    private void Explode()
+    {
+        StartCoroutine(Exploding());
+    }
+
+    private IEnumerator Exploding()
+    {
+        smoke.Play();
+        yield return new WaitForSeconds(delayBeforeExplosion);
+        smoke.Stop();
+        ps.Play();
+        coll.enabled = true;
+        yield return new WaitForSeconds(duration);
+        ps.Stop();
+        coll.enabled = false;
+        Destroy(gameObject, 2);
+    }
+}
