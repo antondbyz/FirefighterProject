b6183aee0ad0b84ae403d76b5ba0a101cd14beb2
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-01-21 21:26:17 +0200

    Added gas pipe

---
+++ Assets/Scripts/Miscellaneous/SwitchableParticleSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SwitchableParticleSystem : MonoBehaviour
+{
+    [SerializeField] private float playDuration = 1;
+    [SerializeField] private float stopDuration = 1;
+
+    private ParticleSystem ps;
+
+    private IEnumerator Start() 
+    {
+        ps = GetComponent<ParticleSystem>();
+        WaitForSeconds playDelay = new WaitForSeconds(stopDuration);
+        WaitForSeconds stopDelay = new WaitForSeconds(playDuration);
+        while(true)
+        {
+            ps.Play();
+            yield return stopDelay;
+            ps.Stop();
+            yield return playDelay;    
+        }
+    }
+}
