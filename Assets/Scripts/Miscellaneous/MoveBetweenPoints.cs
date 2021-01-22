cc8bb1691ed302e4b32743400decb176d0db696e
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-01-22 20:52:43 +0200

    Cleaned project

---
+++ Assets/Scripts/Miscellaneous/MoveBetweenPoints.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MoveBetweenPoints : MonoBehaviour
+{
+    [SerializeField] private Transform[] points = null;
+    [SerializeField] private float speed = 10;
+
+    private Vector2 nextPoint;
+    private Transform myTransform;
+    private int nextPointIndex = 0;
+
+    private void Awake() 
+    {
+        nextPoint = points[nextPointIndex].position;
+        myTransform = transform;    
+    }
+
+    private void Update() 
+    {
+        myTransform.position = Vector2.MoveTowards(myTransform.position, nextPoint, speed * Time.deltaTime);
+        if((Vector2)myTransform.position == nextPoint)
+        {
+            nextPointIndex++;
+            if(nextPointIndex >= points.Length) nextPointIndex = 0;
+            nextPoint = points[nextPointIndex].position;
+        }
+    }
+}
