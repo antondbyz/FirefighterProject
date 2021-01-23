d1896746c09477f27894116f31f2d04830c85145
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-01-23 13:08:20 +0200

    Cleaned project

--- Assets/Scripts/Breakable/Wall/BrokenWall.cs
+++ Assets/Scripts/Breakable/Wall/BrokenWall.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BrokenWall : MonoBehaviour 
 {
-    public Transform ChildFire { get; set; }
+    [HideInInspector] public Transform ChildFire;
 
     [SerializeField] private float lifetime = 1;
     [SerializeField] private ParticleSystem wallFellEffect = null;
@@ -17,7 +17,7 @@ public class BrokenWall : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other) 
     {
-        ChildFire?.SetParent(null, true); 
+        if(ChildFire != null) ChildFire.SetParent(null, true); 
         ParticleSystem newEffect = Instantiate(wallFellEffect, myTransform.position, Quaternion.identity);
         GameController.DestroyWithDelay(newEffect.gameObject, 2);
         Destroy(gameObject);
