6e49bfca19767f52b81078a3f538b8d1e1a139a0
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-02-09 17:29:08 +0200

    Finished electric shot

---
+++ Assets/Scripts/Miscellaneous/ElectricShot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class ElectricShot : MonoBehaviour
+{
+    [Tooltip("Collider active time relative to the particle lifetime")]
+    [Range(0, 1)] [SerializeField] private float lifetimeMultiplier = 1;
+
+    private IEnumerator Start() 
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        BoxCollider2D myCollider = GetComponent<BoxCollider2D>();    
+        MainModule main = ps.main;
+        float colliderLifetime = main.startLifetime.constant * lifetimeMultiplier;
+        WaitForSeconds disableColliderDelay = new WaitForSeconds(colliderLifetime);
+        WaitForSeconds playEffectDelay = new WaitForSeconds(main.duration - colliderLifetime);
+        while(true)
+        {
+            ps.Play();
+            myCollider.enabled = true;
+            yield return disableColliderDelay;
+            myCollider.enabled = false;
+            yield return playEffectDelay;
+        }
+    }
+}
