32adf31586d87d75ec2ec24c6d03b5cb80b3f42a
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-02-09 22:31:32 +0200

    Cleaned project; added ShootingWire sprite and prefab

--- Assets/Scripts/Miscellaneous/ElectricShot.cs
+++ Assets/Scripts/Miscellaneous/ElectricShot.cs
@@ -5,16 +5,16 @@ using static UnityEngine.ParticleSystem;
 public class ElectricShot : MonoBehaviour
 {
     [Tooltip("Collider active time relative to the particle lifetime")]
-    [Range(0, 1)] [SerializeField] private float lifetimeMultiplier = 1;
+    [Range(0, 1)] [SerializeField] private float colliderActiveTime = 1;
 
     private IEnumerator Start() 
     {
         ParticleSystem myParticles = GetComponent<ParticleSystem>();
         BoxCollider2D myCollider = GetComponent<BoxCollider2D>();    
         MainModule main = myParticles.main;
-        float colliderLifetime = main.startLifetime.constant * lifetimeMultiplier;
-        WaitForSeconds disableColliderDelay = new WaitForSeconds(colliderLifetime);
-        WaitForSeconds playEffectDelay = new WaitForSeconds(main.duration - colliderLifetime);
+        float colliderDuration = main.startLifetime.constant * colliderActiveTime;
+        WaitForSeconds disableColliderDelay = new WaitForSeconds(colliderDuration);
+        WaitForSeconds playEffectDelay = new WaitForSeconds(main.duration - colliderDuration);
         while(true)
         {
             myParticles.Play();
