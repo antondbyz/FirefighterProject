91a535df7ff276842d99e19f3fb0a5dd97d1ce5a
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-02-18 14:12:56 +0200

    Started working on player skins

---
+++ Assets/ScriptableObjects/PlayerSkin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "My assets/New player skin")]
+public class PlayerSkin : ScriptableObject 
+{
+    public Sprite Head;   
+    public Sprite Body;   
+    public Sprite RightArm;   
+    public Sprite LeftArm;   
+    public Sprite RightLeg;   
+    public Sprite LeftLeg;   
+}
---
+++ Assets/Scripts/Managers/PlayerSkinsManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerSkinsManager : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer head = null;
+    [SerializeField] private SpriteRenderer body = null;
+    [SerializeField] private SpriteRenderer rightArm = null;
+    [SerializeField] private SpriteRenderer leftArm = null;
+    [SerializeField] private SpriteRenderer rightLeg = null;
+    [SerializeField] private SpriteRenderer leftLeg = null;
+
+    private PlayerSkin[] skins;
+    private int currentSkinIndex = 0;
+
+    private void Awake() 
+    {
+        skins = Resources.LoadAll<PlayerSkin>("PlayerSkins");
+        UpdateSkin();    
+    }
+
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            if(currentSkinIndex >= skins.Length - 1) currentSkinIndex = 0;
+            else currentSkinIndex++;
+            UpdateSkin();
+        }
+    }
+
+    private void UpdateSkin()
+    {
+        head.sprite = skins[currentSkinIndex].Head;
+        body.sprite = skins[currentSkinIndex].Body;
+        rightArm.sprite = skins[currentSkinIndex].RightArm;
+        leftArm.sprite = skins[currentSkinIndex].LeftArm;
+        rightLeg.sprite = skins[currentSkinIndex].RightLeg;
+        leftLeg.sprite = skins[currentSkinIndex].LeftLeg;
+    }
+}
