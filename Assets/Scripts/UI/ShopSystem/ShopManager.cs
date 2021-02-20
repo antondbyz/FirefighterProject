b3b8899f0879313b8b02a6380e67d59704f5d352
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-02-20 20:52:53 +0200

    Working on shop system

--- Assets/Scripts/UI/ShopSystem/ShopManager.cs
+++ Assets/Scripts/UI/ShopSystem/ShopManager.cs
@@ -1,10 +1,11 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class ShopManager : MonoBehaviour 
 {
     private static ShopItem[] items;
-    private static int[] purchasedItemsIndexes = new int[0];
+    private static List<int> purchasedItemsIndexes = new List<int>(){ 0 };
     private static int usingItemIndex = 0;
 
     [SerializeField] private Transform itemsList = null;
@@ -23,6 +24,61 @@ public class ShopManager : MonoBehaviour
         {
             items[i].Selected = (i == index);
         }
+        UpdateButton();
+    }
+
+    public void BuySelectedItem()
+    {
+        items[selectedItemIndex].CurrentState = ShopItem.State.PURCHASED;
+        purchasedItemsIndexes.Add(selectedItemIndex);
+        UpdateButton();
+    }
+
+    public void UseSelectedItem()
+    {
+        items[usingItemIndex].CurrentState = ShopItem.State.PURCHASED;
+        items[selectedItemIndex].CurrentState = ShopItem.State.USING;
+        usingItemIndex = selectedItemIndex;
+        GameManager.CurrentPlayerSkin = items[usingItemIndex].Skin;
+        UpdateButton();
+    }
+
+    private void Awake() 
+    {
+        items = new ShopItem[GameManager.PlayerSkins.Length];
+        for(int i = 0; i < items.Length; i++)
+        {
+            ShopItem newItem = Instantiate(item, itemsList);
+            newItem.Initialize(GameManager.PlayerSkins[i], i);
+            items[i] = newItem;
+        }
+        for(int i = 0; i < purchasedItemsIndexes.Count; i++)
+        {
+            items[purchasedItemsIndexes[i]].CurrentState = ShopItem.State.PURCHASED;
+        }
+        items[usingItemIndex].CurrentState = ShopItem.State.USING;
+        earnedMoney.text = GameManager.PlayerBalance.ToString();
+    }
+
+    private void OnEnable() 
+    {
+        for(int i = 0; i < items.Length; i++)
+        {
+            items[i].Clicked += SelectItem;
+        }
+        SelectItem(usingItemIndex);
+    }
+
+    private void OnDisable() 
+    {
+        for(int i = 0; i < items.Length; i++)
+        {
+            items[i].Clicked -= SelectItem;
+        }
+    }
+
+    private void UpdateButton()
+    {
         switch(items[selectedItemIndex].CurrentState)
         {
             case ShopItem.State.DEFAULT: 
@@ -46,40 +102,6 @@ public class ShopManager : MonoBehaviour
                 useButton.SetActive(false);
                 break;
             }
-        }
-    }
-
-    private void Awake() 
-    {
-        items = new ShopItem[GameManager.PlayerSkins.Length];
-        for(int i = 0; i < GameManager.PlayerSkins.Length; i++)
-        {
-            ShopItem newItem = Instantiate(item, itemsList);
-            newItem.Initialize(GameManager.PlayerSkins[i], i);
-            items[i] = newItem;
-        }
-        for(int i = 0; i < purchasedItemsIndexes.Length; i++)
-        {
-            items[purchasedItemsIndexes[i]].CurrentState = ShopItem.State.PURCHASED;
-        }
-        items[usingItemIndex].CurrentState = ShopItem.State.USING;
-        earnedMoney.text = GameManager.PlayerBalance.ToString();
-    }
-
-    private void OnEnable() 
-    {
-        for(int i = 0; i < items.Length; i++)
-        {
-            items[i].Clicked += SelectItem;
-        }
-        SelectItem(0);
-    }
-
-    private void OnDisable() 
-    {
-        for(int i = 0; i < items.Length; i++)
-        {
-            items[i].Clicked -= SelectItem;
         }
     }
 }
