f223624e1cc01f4e473d71778c020d11b708c9a9
Anton <48963250+AntonDBYZ@users.noreply.github.com>
2021-03-19 20:59:34 +0200

    Added running sound; created PlayerAudio.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -38,7 +38,6 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Checkpoint currentCheckpoint = null;
     [SerializeField] private ParticleSystem bloodEffect = null;
-    [SerializeField] private Sound[] deathSounds = null;
     [SerializeField] private TMP_Text lifesLeftText = null;
     [SerializeField] private TMP_Text earnedMoneyText = null;
 
@@ -53,7 +52,6 @@ public class Player : MonoBehaviour
             bloodEffect.transform.position = myTransform.position;
             bloodEffect.Play();
             LifesLeft--;
-            deathSounds[Random.Range(0, deathSounds.Length)].Source.Play();
             Died?.Invoke();
         }
     }
---
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] private Extinguisher extinguisher = null;
+    [SerializeField] private Sound[] deathSounds = null;
+    [SerializeField] private Sound runningSound = null;
+    [SerializeField] private Sound extinguishingSound = null;
+
+    private PlayerController controller;
+    private Player player;
+
+    private void Awake() 
+    {
+        controller = GetComponent<PlayerController>();   
+        player = GetComponent<Player>(); 
+    }
+
+    private void OnEnable() => player.Died += PlayRandomDeathSound;
+
+    private void OnDisable() 
+    {
+        player.Died -= PlayRandomDeathSound;
+        if(runningSound.Source != null) runningSound.Source.Stop();    
+        if(extinguishingSound.Source != null) extinguishingSound.Source.Stop();
+    }
+
+    private void Update() 
+    {
+        if(controller.NewVelocity.x != 0 && controller.IsGrounded && !GameController.Instance.IsPaused)
+        {
+            if(!runningSound.Source.isPlaying) runningSound.Source.Play();
+        }
+        else runningSound.Source.Stop();    
+        if(extinguisher.IsTurnedOn) 
+        {
+            if(!extinguishingSound.Source.isPlaying) extinguishingSound.Source.Play();
+        }
+        else extinguishingSound.Source.Stop();
+    }
+
+    private void PlayRandomDeathSound() => deathSounds[Random.Range(0, deathSounds.Length)].Source.Play();
+}
